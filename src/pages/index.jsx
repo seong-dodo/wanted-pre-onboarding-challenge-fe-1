7d5f98f116fac1b070f2fa8b83fa930d9c1b4fd8
seong-dodo <dyrnxm52@hanmail.net>
2022-08-08 22:21:49 +0900

    main page 컴포넌트 설계

---
+++ src/pages/index.jsx
@@ -0,0 +1,19 @@
+const MainPage = () => {
+  return (
+    <div>
+      <h1>
+        <a href="/"> 
+          프리온보딩 챌린지 프로젝트
+        </a>
+      </h1>
+      <div>
+        <div>8월 사전과제</div>
+        <a  href="/login">로그인하러 가기</a>
+        <a  href="/signUp">회원가입</a>
+        <a  href="/todo">할일</a>
+      </div>
+    </div>
+  )
+}
+
+export default MainPage;
