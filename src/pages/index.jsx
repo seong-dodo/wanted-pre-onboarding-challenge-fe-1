ffc05644e9455992854ff3e6d11adbb37f00bd86
seong-dodo <dyrnxm52@hanmail.net>
2022-08-14 20:47:07 +0900

    메인 페이지

--- src/pages/index.jsx
+++ src/pages/index.jsx
@@ -1,19 +1,26 @@
+import React from "react";
+
 const MainPage = () => {
   return (
-    <div>
-      <h1>
-        <a href="/"> 
-          프리온보딩 챌린지 프로젝트
-        </a>
-      </h1>
-      <div>
-        <div>8월 사전과제</div>
-        <a  href="/login">로그인하러 가기</a>
-        <a  href="/signUp">회원가입</a>
-        <a  href="/todo">할일</a>
-      </div>
+    <div className="main">
+      <header className="header">
+        <h1>
+          <a href="/">프리온보딩 챌린지 프로젝트</a>
+        </h1>
+      </header>
+      <ul className="navBar">
+        <li className="navBar-item">
+          <a href="/todo">할일</a>
+        </li>
+        <li className="navBar-item">
+          <a href="/login">로그인</a>
+        </li>
+        <li className="navBar-item">
+          <a href="/signUp">회원가입</a>
+        </li>
+      </ul>
     </div>
-  )
-}
+  );
+};
 
 export default MainPage;
